7e1ad146acac933375bb1e0991d7650f7587978a
AustinxEwell <austin.ewell86@gmail.com>
2021-12-27 11:59:03 -0700

    reciving data from the the server and displaying it back to the client

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
 var server = app.listen(3001, function(){
-    console.log('listening to request on port 3001')
+    console.log('listening to request on port 3001');
 })
 
 //Static Fiiles
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+
+    //listen for the message being sent to the server
+    socket.on('chat', function(data){
+        io.emit('chat', data);
+    });
+});
